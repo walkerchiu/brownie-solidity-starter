5d45cd6f76567200d4d16307493a4ea59fac4199
walkerchiu <chenjen.chiou@gmail.com>
2023-05-10 22:07:43 +0800

    Refactor(Module): Refactor SimpleNFT contract deployment using separate functions

--- scripts/deploy_simple_nft.py
+++ scripts/deploy_simple_nft.py
@@ -10,17 +10,17 @@ def main():
         print(f"Deployer address: {deployer.address}\n")
 
         # Get the ETH balance before contract deployment
-        balance_before = deployer.balance()
+        balance_before = get_balance(deployer)
         print(f"Balance before deployment: {balance_before}\n")
 
         # Deploy the contract
-        simple_nft = SimpleNFT.deploy({"from": deployer})
+        simple_nft = deploy_simple_nft(deployer)
 
         # Print the contract address
         print(f"SimpleNFT deployed at: {simple_nft.address}\n")
 
         # Get the ETH balance after contract deployment
-        balance_after = deployer.balance()
+        balance_after = get_balance(deployer)
         print(f"Balance after deployment: {balance_after}\n")
 
         # Calculate the ETH cost of contract deployment
@@ -28,6 +28,32 @@ def main():
         print(f"Deployment cost: {deployment_cost} wei\n")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def deploy_simple_nft(deployer):
+    """
+    Deploy the SimpleNFT contract.
+
+    :param deployer: The account used for deployment.
+    :return: The deployed SimpleNFT contract object.
+    """
+    try:
+        # Deploy the contract
+        simple_nft = SimpleNFT.deploy({"from": deployer})
+        return simple_nft
+    except Exception as e:
+        print(f"Error deploying contract: {e}")
+        return None
+
+
+def get_balance(account):
+    """
+    Get the ETH balance of an account.
+
+    :param account: The account to check the balance.
+    :return: The ETH balance of the account.
+    """
+    return account.balance()
 
 
 if __name__ == "__main__":
